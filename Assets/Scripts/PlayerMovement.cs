b4aa8812295fb38627f69a285cdbb112dbbec279
Soham Ovhal <ovhalsoham@gmail.com>
2023-12-17 13:28:07 +0530

    PlayerMovement
    Made the player movement much better

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,36 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5f;
-    // Start is called before the first frame update
+    public float speed = 10f;
+    private Rigidbody2D rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //Player Movement script
-        float x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-        float y = Input.GetAxis("Vertical") * Time.deltaTime * speed;
-        transform.Translate(x, y, 0);
+        // Get input values
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(horizontalInput, verticalInput);
+        movement.Normalize(); // Ensure diagonal movement isn't faster
+
+        // Move the player
+        rb.velocity = movement * speed;
+
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        if(col.gameObject.tag == "BoxDoor")
+        if (col.gameObject.tag == "BoxDoor")
         {
-            //Debug.Log("BoxDoor enter");
             SceneManager.LoadScene(1);
         }
         else if (col.gameObject.tag == "ToyDoor")
         {
-            //Debug.Log("ToyDoor enter");
             SceneManager.LoadScene(2);
         }
     }

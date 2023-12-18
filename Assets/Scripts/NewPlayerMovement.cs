12d5891917cf4e236670f221341157a7cbe58d28
Sahil Dhumane <sahilpdhumane@gmail.com>
2023-12-18 09:57:00 +0530

    Added Player
    added Player and Player Animations

---
+++ Assets/Scripts/NewPlayerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NewPlayerMovement : MonoBehaviour
+{
+    private Vector2 movement;
+    private Rigidbody2D rb;
+    [Range(2,10)] [SerializeField] private float speed = 3.8f;
+    private Animator animator;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnMovement(InputValue value)
+    {
+        movement = value.Get<Vector2>();
+
+        if (movement.x != 0 || movement.y != 0) {
+            animator.SetFloat("X", movement.x);
+            animator.SetFloat("Y", movement.y);
+
+            animator.SetBool("IsWalking", true);
+        }
+        else {
+            animator.SetBool ("IsWalking", false);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}

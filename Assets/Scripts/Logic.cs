2af925d4dd260b14219a1f7845417e2f05891c1e
Soham Ovhal <ovhalsoham@gmail.com>
2023-12-21 17:32:40 +0530

    newLogicApplied

--- Assets/Scripts/Logic.cs
+++ Assets/Scripts/Logic.cs
@@ -1,3 +1,4 @@
+/*
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -144,4 +145,151 @@ public class Logic : MonoBehaviour
 
 
     }
+} */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Logic : MonoBehaviour
+{
+    private Camera _mainCamera;
+    public GameObject product;
+    SpriteRenderer rnd;
+    public string s, initSprite;
+    public bool f1, f2, f3;
+    public bool productAtPickUp;
+    public Sprite[] spriteArray;
+    //public Vector2 m2pos;
+    //moveM1toM2 m1tom2;
+    void Start()
+    {
+        product.SetActive(false);
+        f1 = true;
+        f2 = false;
+        f3 = false;
+        productAtPickUp = false;
+        rnd = product.GetComponent<SpriteRenderer>();
+        // .GetComponent<YourScriptName>.YourFunction()
+        //m1tom2 = FindObjectOfType<moveM1toM2>();
+        //moveit();
+    }
+    void Awake()
+    {
+        _mainCamera = Camera.main;
+        s = "";
+        initSprite = "";
+    }
+    //     public void moveit()
+    //     {
+    //         m1tom2.move();
+
+    //     }
+    public void OnClick(InputAction.CallbackContext context)
+    {
+        if (!context.started) return;
+        var rayHit = Physics2D.GetRayIntersection(_mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue()));
+        if (!rayHit.collider) return;
+        var selectedOption = rayHit.collider.gameObject.name;
+        switch (selectedOption)
+        {
+            case "slot1":
+                if (!f1) break;
+                Debug.Log("slot1 Selected");
+                product.SetActive(true);
+                rnd.sprite = spriteArray[0];
+                product.GetComponent<move>().moveM1toM2();
+                s = "Sq";
+                initSprite = "Sq";
+                //Invoke("DelayedFunction",2f);
+                //productSquare.transform.Translate(Vector3.right * Time.deltaTime, Space.World);
+                //moveit();
+
+                f1 = false;
+                f2 = true;
+                break;
+            case "slot2":
+                if (!f1) break;
+                Debug.Log("slot2 Selected");
+                product.SetActive(true);
+                rnd.sprite = spriteArray[0];
+                product.GetComponent<move>().moveM1toM2();
+                s = "Cy";
+                initSprite = "Ci";
+                f1 = false;
+                f2 = true;
+                break;
+            case "slot3":
+                if (!f1) break;
+                Debug.Log("slot3 Selected");
+                product.SetActive(true);
+                rnd.sprite = spriteArray[0];
+                product.GetComponent<move>().moveM1toM2();
+                s = "Lo";
+                initSprite = "Ca";
+                f1 = false;
+                f2 = true;
+                break;
+            case "slot4":
+                if (!f2) break;
+                Debug.Log("slot4 Selected");
+                rnd.sprite = spriteArray[0];
+                product.GetComponent<move>().moveM2toM3();
+                s += "Re";
+                f2 = false;
+                f3 = true;
+                break;
+            case "slot5":
+                if (!f2) break;
+                Debug.Log("slot5 Selected");
+                rnd.sprite = spriteArray[0];
+                product.GetComponent<move>().moveM2toM3();
+                s += "Gr";
+                f2 = false;
+                f3 = true;
+                break;
+            case "slot6":
+                if (!f2) break;
+                Debug.Log("slot6 Selected");
+                rnd.sprite = spriteArray[0];
+                product.GetComponent<move>().moveM2toM3();
+                s += "Bl";
+                f2 = false;
+                f3 = true;
+                break;
+            case "slot7":
+                if (!f3) break;
+                Debug.Log("slot7 Selected");
+                rnd.sprite = spriteArray[0];
+                product.GetComponent<move>().moveM3toPickup();
+                s += "P2";
+                f3 = false;
+                productAtPickUp = true;
+                f1 = true;
+                break;
+            case "slot8":
+                if (!f3) break;
+                Debug.Log("slot8 Selected");
+                rnd.sprite = spriteArray[0];
+                product.GetComponent<move>().moveM3toPickup();
+                s += "P1";
+                f3 = false;
+                productAtPickUp = true;
+                f1 = true;
+                break;
+            case "slot9":
+                if (!f3) break;
+                Debug.Log("slot9 Selected");
+                rnd.sprite = spriteArray[0];
+                product.GetComponent<move>().moveM3toPickup();
+                s += "P3";
+                f3 = false;
+                productAtPickUp = true;
+                f1 = true;
+                break;
+
+        }
+    }
 }
+
--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -4,17 +4,14 @@ using UnityEngine;
 
 public class Pickup : MonoBehaviour
 {
-    public GameObject productSquare,productCircle,productCapsule;
+    public GameObject product;
     public GameObject Logic;
     void OnCollisionEnter2D(Collision2D collision)
     {
         if(Logic.GetComponent<Logic>().productAtPickUp)
         {
-
             Logic.GetComponent<Logic>().f1 = true;
-            if(Logic.GetComponent<Logic>().initSprite == "Sq") productSquare.GetComponent<move>().Follow(Logic.GetComponent<Logic>().s);
-            else if(Logic.GetComponent<Logic>().initSprite == "Ci") productCircle.GetComponent<move>().Follow(Logic.GetComponent<Logic>().s);
-            else productCapsule.GetComponent<move>().Follow(Logic.GetComponent<Logic>().s);
+            product.GetComponent<move>().Follow(Logic.GetComponent<Logic>().s);
         }
         //if(collision.gameObject.CompareTag("Player"))
     }
